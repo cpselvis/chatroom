3926ee6250a772ee3c7b50d29fd26dc1df7379b4
cpselvis <cpselvis@gmail.com>
2017-08-20 11:20:39 +0800

    feat: 聊天信息发送面板区域

--- app/components/chat-panel/index.js
+++ app/components/chat-panel/index.js
@@ -4,7 +4,41 @@ import './index.scss';
 class ChatPanel extends Component {
 
   render() {
-    return <div className="test">Chat panel.</div>;
+    return <div className="chat-panel">
+      <div className="header">
+        <span className="nickname">Sophie Marceau</span>
+        <span className="status">
+          <i className="online" />
+          Mobile online
+        </span>
+      </div>
+      <div className="msg-panel">
+        <div className="msg-item">
+          <div className="avatar">
+            <img src="https://avatars0.githubusercontent.com/u/18289264?v=4&u=61082b05c36af8782b9d3abe227b7f429de7b816&s=100" />
+          </div>
+          <div className="msg-text">Diease good? how do you feel?</div>
+        </div>
+        <div className="msg-item">
+          <div className="avatar">
+            <img src="https://avatars0.githubusercontent.com/u/18289264?v=4&u=61082b05c36af8782b9d3abe227b7f429de7b816&s=100" />
+          </div>
+          <div className="msg-text">Diease good? how do you feel?</div>
+        </div>
+        <div className="msg-item">
+          <div className="avatar">
+            <img src="https://avatars0.githubusercontent.com/u/18289264?v=4&u=61082b05c36af8782b9d3abe227b7f429de7b816&s=100" />
+          </div>
+          <div className="msg-text">Diease good? how do you feel?</div>
+        </div>
+        <div className="msg-item">
+          <div className="avatar">
+            <img src="https://avatars0.githubusercontent.com/u/18289264?v=4&u=61082b05c36af8782b9d3abe227b7f429de7b816&s=100" />
+          </div>
+          <div className="msg-text">Diease good? how do you feel?</div>
+        </div>
+      </div>
+    </div>;
   }
 }
 
